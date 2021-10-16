04f7a3bc20e1e87df2c2ab5b8738d928d95f191a
Willian <wnreichert@inf.ufrgs.br>
2021-10-16 15:20:27 -0300

    finished basic functionality

--- user.py
+++ user.py
@@ -1,33 +1,38 @@
 import db
 
 class User:
-    def __init__ (self, name, password):
-        self.name = name
-        self.password = password
+    def __init__ (self):
+        self.name = None
+        self.password = None
+        self.email = None
 
     # print user info
-    def printUser(self):
-        print(self.name+'\n'+self.password)
-        print(f'{self.name}\n{self.password}')
+    def print_user(self):
+        print(f'{self.name}\n{self.password}\n{self.email}')
     
     # create user account
-    def createAccount(self):
-        userData = db.getUser(self.name)
-        print(userData)
-        if not userData:
-            db.insertUser(self)
-            print("Account created")
-        else:
-            print("User already exists :(")
-    
+    def create_account(self):
+        user_data = db.get_user_by_name(self.name)
+        if user_data:
+            print('Nome de usuário já existe')
+            return 1
+        user_data = db.get_user_by_email(self.email)
+        if user_data:
+            print('Email já utilizado')
+            return 1
+        db.insert_user(self)
+        print('Conta criada')
+        return 0
+
     # log user in
     def login(self):
-        userData = db.getUser(self.name)
-        if not userData:
-            print("User not found :(")
-            return
-        password = userData[0][1]
+        user_data = db.get_user_by_name(self.name)
+        if not user_data:
+            print('Usuário não encontrado')
+            return 1
+        password = user_data[1]
         if self.password != password:
-            print("Wrong password :(")
-            return
-        print("Logged in! :)")
+            print('Senha inválida')
+            return 1
+        print('Login realizado')
+        return 0
--- vapor.py
+++ vapor.py
@@ -1,23 +1,39 @@
 from user import User
 import db
 
-OPTIONS = "12"
 
-# get option
-option = input("1 - Sign in\n2 - Sign up\n")
-while option not in OPTIONS:
-    option = input("1 - Sign in\n2 - Sign up\n")
+OPTIONS = '012'
+MENU = '1 - Login\n2 - Criar conta\n0 - Sair\n'
+user = User()
 
-# get user info
-username = input("Type your username:\n")
-password = input("Type your password:\n")
-user = User(username, password)
+def get_login_data():
+    user.name = input('\nUsuário: ')
+    user.password = input('Senha: ')
 
-# execute option
-if option == "1":
-    user.login()
-elif option == "2":
-    user.createAccount()
+def get_account_creation_data():
+    user.name = input('\nUsuário: ')
+    user.password = input('Senha: ')
+    user.email = input('E-mail: ')
 
-# cleanup
-db.close()
+
+if __name__ == '__main__':
+
+    # get option'
+    option = input(MENU)
+    while option not in OPTIONS:
+        option = input(MENU)
+
+    # execute option
+    if option == '1':
+        get_login_data()
+        while (user.login() != 0):
+            get_login_data()
+
+    elif option == '2':
+        get_account_creation_data()
+        while (user.create_account() != 0):
+            get_account_creation_data()
+
+    elif option == '0':
+        db.close()
+        exit(0)
