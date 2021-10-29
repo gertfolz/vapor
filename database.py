eace7bac2d682c9f008dedcea07fabc2692436db
gertfolz <gertfolz@hotmail.com>
2021-10-29 18:08:41 -0300

    novos inserts, classe game.py e jogos.txt

--- database.py
+++ database.py
@@ -1,27 +1,50 @@
 import sqlite3
 
-
 # TODO: utilizar restrições de integridade?
 con = sqlite3.connect('database.db')
 cur = con.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT, email TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS friendship (username1 TEXT, username2 TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS userGames (username TEXT, game TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS game (name TEXT, priceBuy TEXT, priceRent TEXT, idNumber TEXT, desc TEXT, releaseDate TEXT, developer TEXT)')
 con.commit()
 
-def get_user_by_name(username: str):
+def get_user_by_name(username):
     ''' Recupera os dados de um usuário pelo nome de usuário. '''
     cmd = 'SELECT * FROM user WHERE username=?'
     return cur.execute(cmd, (username,)).fetchone()
 
-def get_user_by_email(email: str):
+def get_user_by_email(email):
     ''' Recupera os dados de um usuário pelo e-mail '''
     cmd = 'SELECT * FROM user WHERE email=?'
     return cur.execute(cmd, (email,)).fetchone()
 
-def insert_user(user: object):
+def insert_user(username, password, email):
     ''' Insere uma nova entrada de usuário. '''
     cmd = 'INSERT INTO user VALUES (?, ?, ?)'
-    cur.execute(cmd, (user.username, user.password, user.email))
+    cur.execute(cmd, (username, password, email))
     con.commit()
+
+def insert_friendship(username1, username2):
+    ''' Insere uma nova entrada de amizade entre usuários. '''
+    cmd = 'INSERT INTO friendship VALUES (?, ?)'
+    cur.execute(cmd, (username1, username2))
+    con.commit()
+
+def insert_gameToUserLibrary(username, game):
+    ''' Insere um novo jogo à lista de jogos do usuário. '''
+    cmd = 'INSERT INTO userGames VALUES (?, ?)'
+    cur.execute(cmd, (username, game))
+    con.commit()
+
+def insert_game(name, priceBuy, priceRent, idNumber, desc, releaseDate, developer):
+    '''Insere um novo jogo à lista de jogos na loja. '''
+    cmd = 'INSERT INTO game VALUES (?, ?, ?, ?, ?, ?, ?)'
+    cur.execute(cmd, (name, priceBuy, priceRent, idNumber, desc, releaseDate, developer))
+    con.commit()
+
+
+
 
 # TODO: abrir e fechar antes e depois de cada operação?
 def close():
---
+++ game.py
@@ -0,0 +1,12 @@
+''' Classe que representa um jogo. '''
+from dataclasses import dataclass
+
+
+@dataclass
+class Game:
+    name: str = None # nome do jogo
+    priceBuy: float = None # preço de compra do jogo
+    priceRent: float = None # preço de aluguel do jogo
+    desc: str = None # descrição do jogo
+    releaseDate: str = None # data de lançamento do jogo
+    developer: str = None # desenvolvedor do jogo
